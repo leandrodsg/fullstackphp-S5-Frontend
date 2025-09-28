1d2ccccd0779e37c2068640f9dcaafec55417a19
leandrodsg <leandrodsg.pt@gmail.com>
2025-09-28 17:06:53 +0200

    Create project structure

---
+++ src/utils/helpers.js
@@ -0,0 +1,22 @@
+export const formatCurrency = (amount, currency = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+  }).format(amount);
+};
+
+export const formatDate = (date) => {
+  return new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  });
+};
+
+export const getInitials = (name) => {
+  return name
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase())
+    .join('')
+    .substring(0, 2);
+};
